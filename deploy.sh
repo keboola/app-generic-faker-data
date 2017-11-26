b9e22d990d594010dbb0dffee2dbbe54a824ae80
Odin <odinuv@gmail.com>
2017-11-26 23:47:03 +0100

    cleanup and register task

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,5 @@ docker push quay.io/keboola/app-generic-faker:latest
 pip install awscli --upgrade --user
 
 #aws ecs run-task --cluster ${AWS_CLUSTER} --started-by TravisCI --task-definition generic_faker
-aws ecs update-service --cluster ${AWS_CLUSTER} --service generic_faker --task-definition generic_faker
+aws ecs register-task-definition --cli-input-json task-def.json 
+aws ecs update-service --cluster ${AWS_CLUSTER} --service generic_faker --task-definition generic_faker:${REVISION} --task-definition
