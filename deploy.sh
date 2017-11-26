7cb34be60e7c0a7efa525e57e9683bc6a7b295d0
Odin <odinuv@gmail.com>
2017-11-27 00:16:07 +0100

    fix: get revision

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,7 @@ docker push quay.io/keboola/app-generic-faker:latest
 pip install awscli --upgrade --user
 
 #aws ecs run-task --cluster ${AWS_CLUSTER} --started-by TravisCI --task-definition generic_faker
-aws ecs register-task-definition --cli-input-json file://${TRAVIS_BUILD_DIR}/task-def.json 
+COMMAND_RESULT=$(aws ecs register-task-definition --cli-input-json file://${TRAVIS_BUILD_DIR}/task-def.json)
+REVISION=$(echo ${COMMAND_RESULT} | grep -o '"revision": [0-9]*' | grep -o '[0-9]*')
+echo "Using revision ${REVISION}"
 aws ecs update-service --cluster ${AWS_CLUSTER} --service generic_faker --task-definition generic_faker:${REVISION} --task-definition
