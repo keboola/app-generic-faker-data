de7e9c82b8e14fefa9fd20cb3e0a933058ac0725
Odin <odinuv@gmail.com>
2017-11-26 16:35:03 +0100

    fix: no tags

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ set -e
 
 # deploy to Quay public repository
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag mock-server quay.io/keboola/app-generic-faker:${TRAVIS_TAG}
+#docker tag mock-server quay.io/keboola/app-generic-faker:${TRAVIS_TAG}
 docker tag mock-server quay.io/keboola/app-generic-faker:latest
 docker images
-docker push quay.io/keboola/app-generic-faker:${TRAVIS_TAG}
+#docker push quay.io/keboola/app-generic-faker:${TRAVIS_TAG}
 docker push quay.io/keboola/app-generic-faker:latest
 
 pip install awscli --upgrade --user
